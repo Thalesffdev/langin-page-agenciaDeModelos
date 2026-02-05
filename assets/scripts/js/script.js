e4f202bef74ac5e644024c602c41308af5d1a888
Thalesffdev <thalesffdev@gmail.com>
2026-02-05 15:50:50 -0300

    Adicionando fade effects

--- assets/scripts/js/script.js
+++ assets/scripts/js/script.js
@@ -1,3 +1,18 @@
+/*Observador de elementos Fades*/
+
+const fades = document.querySelectorAll('.fade');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+            observer.unobserve(entry.target);
+        }
+    });
+});
+    
+fades.forEach(el => observer.observe(el));
+
 function initSlider() {
     const container = document.getElementById('lan-slide-area');
     const track = document.getElementById('slide-track');
